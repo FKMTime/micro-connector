3b32fa39af526dd250884c01722e175e49249679
filipton <filipton12@gmail.com>
2024-04-28 18:16:05 +0200

    wip: version enum

--- src/updater.rs
+++ src/updater.rs
@@ -1,7 +1,7 @@
 use crate::{http::EspConnectInfo, structs::TimerPacket};
 use anyhow::Result;
 use axum::extract::ws::{Message, WebSocket};
-use std::{path::PathBuf, time::SystemTime};
+use std::{fmt::Write, ops::RangeBounds, path::PathBuf, time::SystemTime};
 use tracing::{debug, error, info};
 
 const UPDATE_CHUNK_SIZE: usize = 1024 * 4;
@@ -133,4 +133,81 @@ pub async fn update_client(
     }
 
     Ok(true)
+}
+
+/// Inner u128 is calculated version number,
+/// if version is newer, the number should be bigger
+/// * Important: you can't compare between Dev and Stable version!
+#[derive(PartialEq)]
+pub enum Version {
+    /// Like v2.1.0
+    Stable(u128),
+
+    /// Like DV542356675 (epoch build time)
+    Dev(u128),
+
+    /// Any other version string
+    Other,
+}
+
+impl Version {
+    pub fn from_str(string: &str) -> Self {
+        if string.starts_with("v") {
+            let nmb = Self::read_stable_version_nmb(string.trim_start_matches('v'));
+            Self::Stable(nmb)
+        } else if string.starts_with("DV") {
+            let nmb: u128 = string.trim_start_matches("DV").parse().unwrap_or(0);
+            Self::Dev(nmb)
+        } else {
+            Self::Other
+        }
+    }
+
+    /// Check if version provided as input is newer than self
+    pub fn is_version_newer(&self, ver: Self) -> bool {
+        if ver == Version::Other {
+            return false;
+        }
+
+        // Only compare if version variants are the same!
+        if matches!(ver, Version::Stable(..)) && matches!(self, Version::Stable(..)) {
+            return ver.get_nmb() > self.get_nmb();
+        } else if matches!(ver, Version::Dev(..)) && matches!(self, Version::Dev(..)) {
+            return ver.get_nmb() > self.get_nmb();
+        } else {
+            true
+        }
+    }
+
+    fn get_nmb(&self) -> u128 {
+        match self {
+            &Self::Stable(d) => d,
+            &Self::Dev(d) => d,
+            &Self::Other => 0,
+        }
+    }
+
+    fn read_stable_version_nmb(string: &str) -> u128 {
+        let mut tmp = 0;
+        let mut mult = 1;
+
+        for c in string.chars().rev() {
+            if let Some(d) = c.to_digit(10) {
+                tmp += mult * d as u128;
+                mult *= 10;
+            }
+        }
+
+        tmp
+    }
+}
+
+impl std::fmt::Display for Version {
+    fn fmt(&self, f: &mut std::fmt::Formatter<'_>) -> std::fmt::Result {
+        match self {
+            &Version::Stable(nmb) => f.write_fmt(format_args!("Version::Stable({})", nmb)),
+            &Version::Dev(nmb) => f.write_fmt(format_args!("Version::Dev({})", nmb)),
+            &Version::Other => f.write_str("Version::Other"),
+        }
+    }
 }
