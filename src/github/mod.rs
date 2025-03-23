b9e6c94e7296d2e07b5e12faa2dd94e2002d6f9e
filipton <filipton12@gmail.com>
2025-03-23 19:34:33 +0100

    feat: download staff-at firmware

--- src/github/mod.rs
+++ src/github/mod.rs
@@ -4,33 +4,35 @@ mod structs;
 
 const GITHUB_API_URL: &str = "https://api.github.com";
 const GH_OWNER: &str = "FKMTime";
-const GH_REPO: &str = "firmware";
+const REPOS: &[&str] = &["firmware", "staff-at-firmware"];
 
 pub async fn get_releases(client: &reqwest::Client) -> Result<Vec<structs::ReleaseAssetItem>> {
-    let url = format!("{GITHUB_API_URL}/repos/{GH_OWNER}/{GH_REPO}/releases");
+    let mut tmp = Vec::new();
 
-    let res = client
-        .get(&url)
-        .header("Accept", "application/vnd.github+json")
-        .send()
-        .await?;
+    for repo in REPOS {
+        let url = format!("{GITHUB_API_URL}/repos/{GH_OWNER}/{repo}/releases");
 
-    let release: structs::GithubRelease = {
-        let text = res.text().await?;
+        let res = client
+            .get(&url)
+            .header("Accept", "application/vnd.github+json")
+            .send()
+            .await?;
 
-        let json: Vec<structs::GithubRelease> = serde_json::from_str(&text)?;
-        json.iter()
-            .next()
-            .ok_or_else(|| anyhow::anyhow!("No releases found!"))?
-            .to_owned()
-    };
+        let release: structs::GithubRelease = {
+            let text = res.text().await?;
 
-    Ok(release
-        .assets
-        .iter()
-        .map(|x| structs::ReleaseAssetItem {
+            let json: Vec<structs::GithubRelease> = serde_json::from_str(&text)?;
+            json.iter()
+                .next()
+                .ok_or_else(|| anyhow::anyhow!("No releases found!"))?
+                .to_owned()
+        };
+
+        tmp.extend(release.assets.iter().map(|x| structs::ReleaseAssetItem {
             name: x.name.to_string(),
             download_url: x.browser_download_url.to_string(),
-        })
-        .collect())
+        }));
+    }
+
+    Ok(tmp)
 }
